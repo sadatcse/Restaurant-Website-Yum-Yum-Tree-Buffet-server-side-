0a98616e3fc9e46bc291a73fdfdc749cf34483fa
sadat khan <sadatcse@gmail.com>
2023-11-09 10:15:31 +0600

    send to deploy

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.get('/menusearch/:name', async (req, res) => {
 
 
 
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
